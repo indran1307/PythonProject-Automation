8bdb23dff36f27cae91d4b7d2b62959e1ca7f7ea
nin100 <nemallapudi.indrareddy@cambiumnetworks.com>
2026-09-17 16:25:25 +0530

    Added String operations

---
+++ Src/Day-17thSept26/Literals.py
@@ -0,0 +1,8 @@
+str1 = 'hello\nworld'
+print(str1)
+print('hello\tworld')
+
+dir = r"C:\Users\nin100\OneDrive - Cambium Networks Limited\Documents"
+print(dir)
+
+name2 ='Indra reddy'
---
+++ Src/Day-17thSept26/String operations.py
@@ -0,0 +1,12 @@
+name ='indra'
+print(len(name))
+
+print(name.upper())
+print(name.lower())
+print(name.isalpha())
+first_name = 'indra'
+last_name = 'N'
+full_name = first_name + ' ' + last_name
+print(full_name)
+a = None
+print(type(a))
