aba7fa5d18f4da74951fb8138ac7a9e55c9e257a
nin100 <nemallapudi.indrareddy@cambiumnetworks.com>
2026-09-17 16:33:57 +0530

    Added operators

---
+++ Src/Day-17thSept26/Operators.py
@@ -0,0 +1,20 @@
+print(5/5)
+print(5/2)
+
+print(5//2)
+print(5%2)
+
+print(2**3)
+print(2==2)
+print(2==3)
+
+have_luck = False
+print(not have_luck)
+
+'''Logical Operators'''
+print(2>3) #False
+print(2<3) #True
+
+print(2<=2)
+
+
